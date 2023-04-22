05d541f3f0797f61214887d316072ab04f297df2
Igor Shargin <igor20513@gmail.com>
2023-04-22 18:10:52 +0500

    fix: decomposition selectors

---
+++ src/common/selectors/tasks-selectors/selectors.ts
@@ -0,0 +1,3 @@
+import { AppRootStateType } from 'app/store/store'
+
+export const selectTasks = (state: AppRootStateType) => state.tasks
---
+++ src/common/selectors/todolists-selectors/selectors.ts
@@ -0,0 +1,3 @@
+import { AppRootStateType } from 'app/store/store'
+
+export const selectTodolists = (state: AppRootStateType) => state.todolists
