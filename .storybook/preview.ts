18d757745433656d8427de2d31fc9505e46b5c03
Igor Shargin <igor20513@gmail.com>
2023-08-02 12:39:32 +0500

    fix: update storybook config

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import '../src/styles/index.scss'
+import type { Preview } from '@storybook/react'
+const preview: Preview = {
+  parameters: {
+    actions: { argTypesRegex: '^on.*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+  },
+}
+
+export default preview
